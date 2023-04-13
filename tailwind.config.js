35ac8f88e39349579b0210a72062444fbd2c43c2
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-04-13 21:00:00 +0900

    [change] デザインテンプレート導入, tailwindcssが動作しない不具合修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
+  darkMode: 'light',
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js',
+    'preline/dist/*.js',
   ],
+  plugins: [require('preline/plugin')],
 };
