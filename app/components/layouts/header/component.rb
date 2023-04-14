6d5fc2ee00db120ba29a3fecdac80ebc3dd3a137
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-04-14 17:49:17 +0900

    [change] headerコンポーネントをログイン状態に連動させる

--- app/components/layouts/header/component.rb
+++ app/components/layouts/header/component.rb
@@ -1,6 +1,10 @@
 module Layouts
   module Header
     class Component < ViewComponent::Base
+      def initialize(current_user)
+        @current_user = current_user # User < ApplicationRecord
+        super
+      end
     end
   end
 end
