6d0165cb6b05b7bd40d806f3aad04d34b23ab66d
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-04-16 13:24:39 +0900

    [change] UserとTaskモデルを微調整

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true
 
   enum :priority, { low: 0, medium: 1, high: 2 }
-
-  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
+  has_many :tasks, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :tasks, dependent: :destroy
 end
