7f4e62e751191008dc0db3100db9141db29f3e04
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-04-17 09:43:51 +0900

    [change] 検索フォームの仮実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,29 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
 
+  helper_method :search_params
+
   def index
     @tasks = current_user.tasks
+
+    # @tasks = Tasks::SearchFinder.call(current_user.tasks, **search_params)
+  end
+
+  def show; end
+
+  def edit; end
+
+  def create; end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+  def search_params
+    return {} if params[:search].blank?
+
+    params.require(:search).permit(:title, :summary, :due_date, :priority)
   end
 end
