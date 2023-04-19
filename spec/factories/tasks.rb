12ec12900451850f28f741d8e529fbc9aa56d3a8
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-04-19 23:40:01 +0900

    [change] taskモデルのファクトリにアソシエーションを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :task do
+    association :user, factory: :user, strategy: :create
+
     sequence(:title) { |n| "title_#{n}" }
     sequence(:summary) { |n| "summary_#{n}" }
     due_date { "2030-01-01" }
