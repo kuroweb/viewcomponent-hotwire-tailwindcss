fa766109b8430ec8c2f6a2917b4985bca874a218
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-04-19 18:23:14 +0900

    [add] Tasks::UpdateServiceクラスを実装, およびRSpecを実装

--- app/services/tasks/update_service.rb
+++ app/services/tasks/update_service.rb
@@ -1,19 +1,36 @@
 # frozen_string_literal: true
 
+# NOTE: Taskレコードを更新するサービスクラス
 module Tasks
   class UpdateService
     def self.call(...)
       new(...).call
     end
 
-    def initialize(params:)
-      @params = params
+    def initialize(task:, params:)
+      @task = task     # type: Task < ApplicationRecord
+      @params = params # type: TasksController #update_task_params
     end
 
-    def call; end
+    def call
+      return ServiceResponse.error(message: "Tasks::CreateService is failed.", payload: { task: }) if task.nil?
+
+      task.assign_attributes(
+        title: params[:title],
+        summary: params[:summary],
+        due_date: params[:due_date],
+        priority: params[:priority]
+      )
+
+      if task.save
+        ServiceResponse.success(payload: task)
+      else
+        ServiceResponse.error(message: "Tasks::CreateService is failed.", payload: { task: })
+      end
+    end
 
     private
 
-    attr_reader :params
+    attr_reader :task, :params
   end
 end
