efba47ed5b8b5aca98f046f3ef68c9dac35d06f0
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-04-20 02:01:43 +0900

    [change] search_card_componentをリファクタ

--- app/components/tasks/index/search_card_controller.js
+++ app/components/tasks/index/search_card_controller.js
@@ -4,6 +4,7 @@ import { Japanese } from 'flatpickr/dist/l10n/ja';
 
 export default class extends Controller {
   static values = {};
+  static targets = ['startDateField', 'endDateField', 'resetButton'];
 
   connect() {
     const SearchForm = (() => {
@@ -12,32 +13,19 @@ export default class extends Controller {
 
       // 締切日の開始日フォームを初期化
       const initStartDatePicker = () => {
-        flatpickr('#due_date_start_date_field', {
-          dateFormat: 'Y/m/d',
-        });
+        flatpickr(this.startDateFieldTarget, { dateFormat: 'Y/m/d' });
       };
 
       // 締切日の終了日フォームを初期化
       const initEndDatePicker = () => {
-        flatpickr('#due_date_end_date_field', {
-          dateFormat: 'Y/m/d',
-        });
+        flatpickr(this.endDateFieldTarget, { dateFormat: 'Y/m/d' });
       };
 
       // 締切日のリセットボタンを初期化
       const initDueDateResetButton = () => {
-        const buttonElement = document.getElementById('due_date_reset_button');
-
-        buttonElement.addEventListener('click', () => {
-          const startDatePicker = document.getElementById(
-            'due_date_start_date_field'
-          );
-          const endDatePicker = document.getElementById(
-            'due_date_end_date_field'
-          );
-
-          startDatePicker.value = '';
-          endDatePicker.value = '';
+        this.resetButtonTarget.addEventListener('click', () => {
+          this.startDateFieldTarget.value = '';
+          this.endDateFieldTarget.value = '';
         });
       };
 
