8e21490356b7a546b9b2ce842d5d6d57514e06af
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-04-19 18:32:30 +0900

    [add] Tasks::DestroyServiceクラスのRSpecを実装

--- app/services/tasks/destroy_service.rb
+++ app/services/tasks/destroy_service.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# NOTE: Taskレコードを削除するサービスクラス
 module Tasks
   class DestroyService
     def self.call(...)
---
+++ spec/services/tasks/destroy_service_spec.rb
@@ -0,0 +1,31 @@
+#============
+# テストケース
+#============
+# Tasks::DestroyService
+#   #call
+#     taskパラメータが「正常」な場合
+#       「ServiceResponse.success」を返却すること
+#     「taskパラメータ」が不正な場合
+#       「StandardError」を返却すること
+
+require "rails_helper"
+
+RSpec.describe Tasks::DestroyService do
+  describe "#call" do
+    let!(:user) { create(:user) }
+    let!(:task) { create(:task, user:) }
+
+    context "taskパラメータが「正常」な場合" do
+      it "「ServiceResponse.success」を返却すること" do
+        result = described_class.call(task:)
+        expect(result.success?).to be(true)
+      end
+    end
+
+    context "「taskパラメータ」が不正な場合" do
+      it "「StandardError」を返却すること" do
+        expect { described_class.call(task: nil) }.to raise_error(StandardError)
+      end
+    end
+  end
+end
