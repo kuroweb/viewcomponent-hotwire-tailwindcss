606d4e8469f2da3d8277db28bae9d1a168c2d9dd
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-04-20 00:25:12 +0900

    [change] Userモデルのユニットテストを実装

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+#============
+# テストケース
+#============
+# User
+#   association
+#     has_many: tasks
+#       アソシエーション先のTaskモデルを取得できること
+#       Userモデルを削除したときに、Taskモデルも削除されること
+
+require "rails_helper"
+
+RSpec.describe User do
+  let!(:user) { create(:user) }
+  let!(:task) { create(:task, user:) }
+
+  describe "association" do
+    context "has_many: tasks" do
+      it "アソシエーション先のTaskモデルを取得できること" do
+        expect(user.tasks).to include(task)
+      end
+
+      it "Userモデルを削除したときに、Taskモデルも削除されること" do
+        expect { user.destroy }.to change(Task, :count).by(-1)
+      end
+    end
+  end
+end
