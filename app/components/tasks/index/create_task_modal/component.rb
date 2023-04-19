f1fe53a19b51b0cfbb0e613f4b3a6b1c0dcb57c8
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-04-19 12:25:10 +0900

    [change] タスク作成モーダルの実装を修正

--- app/components/tasks/index/create_task_modal/component.rb
+++ app/components/tasks/index/create_task_modal/component.rb
@@ -2,6 +2,10 @@ module Tasks
   module Index
     module CreateTaskModal
       class Component < ViewComponent::Base
+        def initialize(element_id)
+          @element_id = element_id # type: string
+          super
+        end
       end
     end
   end
