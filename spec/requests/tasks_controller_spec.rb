6ac2ba2ae0cd4f50b39c854c4150a5425c6eedfd
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-04-27 02:01:47 +0900

    [add] TasksControllerのユニットテストを追加

--- spec/requests/tasks_controller_spec.rb
+++ spec/requests/tasks_controller_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Tasks, type: :request do
+RSpec.describe Tasks do
   let!(:user) { create(:user) }
 
   describe "GET #index" do
@@ -15,7 +15,7 @@ RSpec.describe Tasks, type: :request do
   end
 
   describe "POST #create" do
-    let!(:create_task_params) { attributes_for(:task) }
+    let!(:create_task_params) { attributes_for(:task).merge(user:) }
 
     before do
       sign_in user
@@ -34,6 +34,55 @@ RSpec.describe Tasks, type: :request do
 
     it "tasks_pathにリダイレクトされること" do
       post tasks_path, params: { task: create_task_params }
+      expect(response).to redirect_to tasks_path
+    end
+  end
+
+  describe "PUT #update" do
+    let!(:task) { create(:task, user:, title: "title") }
+    let!(:update_task_params) { attributes_for(:task, title: "hoge") }
+
+    before do
+      sign_in user
+    end
+
+    it "リクエストが成功すること" do
+      put task_path(task.id), params: { task: update_task_params }
+      expect(response).to have_http_status(:found)
+    end
+
+    it "タスクのタイトルが更新されること" do
+      expect do
+        put task_path(task.id), params: { task: update_task_params }
+        task.reload
+      end.to change(task, :title).from("title").to("hoge")
+    end
+
+    it "tasks_pathにリダイレクトされること" do
+      put task_path(task.id), params: { task: update_task_params }
+      expect(response).to redirect_to tasks_path
+    end
+  end
+
+  describe "DELETE #destroy" do
+    before do
+      sign_in user
+      create(:task, user:)
+    end
+
+    it "リクエストが成功すること" do
+      delete task_path(Task.last.id)
+      expect(response).to have_http_status(:found)
+    end
+
+    it "タスクが削除されること" do
+      expect do
+        delete task_path(Task.last.id)
+      end.to change(Task, :count).by(-1)
+    end
+
+    it "tasks_pathにリダイレクトされること" do
+      delete task_path(Task.last.id)
       expect(response).to redirect_to tasks_path
     end
   end
