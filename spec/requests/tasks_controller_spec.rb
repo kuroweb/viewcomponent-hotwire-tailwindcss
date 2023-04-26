c193e70e9c6082978d26e5030b91d64d5990bfd2
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-04-27 02:04:27 +0900

    [add] TasksControllerのテストケースを追加

--- spec/requests/tasks_controller_spec.rb
+++ spec/requests/tasks_controller_spec.rb
@@ -15,52 +15,64 @@ RSpec.describe Tasks do
   end
 
   describe "POST #create" do
-    let!(:create_task_params) { attributes_for(:task).merge(user:) }
+    context "リクエスト内容が正常な場合" do
+      let!(:create_task_params) { attributes_for(:task).merge(user:) }
 
-    before do
-      sign_in user
-    end
+      before do
+        sign_in user
+      end
 
-    it "リクエストが成功すること" do
-      post tasks_path, params: { task: create_task_params }
-      expect(response).to have_http_status(:found)
-    end
-
-    it "タスクが追加されること" do
-      expect do
+      it "リクエストが成功すること" do
         post tasks_path, params: { task: create_task_params }
-      end.to change(Task, :count).by(1)
+        expect(response).to have_http_status(:found)
+      end
+
+      it "タスクが追加されること" do
+        expect do
+          post tasks_path, params: { task: create_task_params }
+        end.to change(Task, :count).by(1)
+      end
+
+      it "tasks_pathにリダイレクトされること" do
+        post tasks_path, params: { task: create_task_params }
+        expect(response).to redirect_to tasks_path
+      end
     end
 
-    it "tasks_pathにリダイレクトされること" do
-      post tasks_path, params: { task: create_task_params }
-      expect(response).to redirect_to tasks_path
+    context "リクエスト内容が不正な場合" do
+      # 未実装
     end
   end
 
   describe "PUT #update" do
-    let!(:task) { create(:task, user:, title: "title") }
-    let!(:update_task_params) { attributes_for(:task, title: "hoge") }
+    context "リクエスト内容が正常な場合" do
+      let!(:task) { create(:task, user:, title: "title") }
+      let!(:update_task_params) { attributes_for(:task, title: "hoge") }
 
-    before do
-      sign_in user
-    end
+      before do
+        sign_in user
+      end
 
-    it "リクエストが成功すること" do
-      put task_path(task.id), params: { task: update_task_params }
-      expect(response).to have_http_status(:found)
-    end
-
-    it "タスクのタイトルが更新されること" do
-      expect do
+      it "リクエストが成功すること" do
         put task_path(task.id), params: { task: update_task_params }
-        task.reload
-      end.to change(task, :title).from("title").to("hoge")
+        expect(response).to have_http_status(:found)
+      end
+
+      it "タスクのタイトルが更新されること" do
+        expect do
+          put task_path(task.id), params: { task: update_task_params }
+          task.reload
+        end.to change(task, :title).from("title").to("hoge")
+      end
+
+      it "tasks_pathにリダイレクトされること" do
+        put task_path(task.id), params: { task: update_task_params }
+        expect(response).to redirect_to tasks_path
+      end
     end
 
-    it "tasks_pathにリダイレクトされること" do
-      put task_path(task.id), params: { task: update_task_params }
-      expect(response).to redirect_to tasks_path
+    context "リクエスト内容が不正な場合" do
+      # 未実装
     end
   end
 
