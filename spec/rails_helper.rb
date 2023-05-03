b5953678266d8cf86e65503eb340af2b243e76d4
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-05-03 12:57:48 +0900

    [clean] rails_helperの記述を整理する

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,15 +1,12 @@
 ENV["RAILS_ENV"] ||= "test"
+abort("The Rails environment is running in production mode!") if Rails.env.production?
 
 require "spec_helper"
+require_relative "../config/environment"
+
 require "rspec/rails"
 require "capybara/rspec"
 require "view_component/test_helpers"
-
-require_relative "../config/environment"
-
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-# Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
